c397169b5c5fa50e27f840d96ad5435caf18ccde
miavai649 <mahmudulhaquenoor@gmail.com>
2025-01-02 11:42:00 +0600

    custom drawer added into menu drawer

--- components/navbar/MenuDrawer.tsx
+++ components/navbar/MenuDrawer.tsx
@@ -1,73 +1,75 @@
 import SvgIcon from '@/assets/Icons'
 import React, { useState } from 'react'
-import { Pressable } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native'
 import { Drawer, DrawerBackdrop, DrawerContent } from '../ui/drawer'
 import { VStack } from '../ui/vstack'
 import { Text } from '../ui/text'
+import CustomDrawer from '../drawer'
+
+const MenuDrawerContent = () => {
+  return (
+    <VStack className='gap-[15px] p-6'>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>Home</Text>
+      </Pressable>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>Categories</Text>
+      </Pressable>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>Shop</Text>
+      </Pressable>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>Blog</Text>
+      </Pressable>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>Contact us</Text>
+      </Pressable>
+      <Pressable
+        className='border-b-[1px] h-10'
+        style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
+        <Text className='text-lg'>About us</Text>
+      </Pressable>
+    </VStack>
+  )
+}
 
 const MenuDrawer = ({ combinedHeight }: any) => {
-  const [showDrawer, setShowDrawer] = useState(false)
+  const [menuDrawer, setMenuDrawer] = useState(false)
   return (
     <>
-      <Pressable
-        onPress={() => {
-          setShowDrawer(true)
-        }}>
-        <SvgIcon iconName='menu' />
-      </Pressable>
-      <Drawer
-        isOpen={showDrawer}
-        onClose={() => {
-          setShowDrawer(false)
-        }}
-        size='lg'
-        anchor='left'>
-        <DrawerBackdrop style={{ backgroundColor: 'transparent' }} />
-        <DrawerContent
-          style={{
-            position: 'absolute',
-            top: combinedHeight,
-            left: 0,
-            width: '80%',
-            backgroundColor: '#fff',
-            padding: 0
-          }}>
-          <VStack className='gap-[15px] p-6'>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>Home</Text>
-            </Pressable>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>Categories</Text>
-            </Pressable>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>Shop</Text>
-            </Pressable>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>Blog</Text>
-            </Pressable>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>Contact us</Text>
-            </Pressable>
-            <Pressable
-              className='border-b-[1px] h-10'
-              style={{ borderColor: 'rgba(85, 83, 83, 0.2)' }}>
-              <Text className='text-lg'>About us</Text>
-            </Pressable>
-          </VStack>
-        </DrawerContent>
-      </Drawer>
+      <CustomDrawer
+        openDrawerTriggerFunc={() => setMenuDrawer(true)}
+        triggerBtnContent={<SvgIcon iconName='menu' />}
+        drawerContent={<MenuDrawerContent />}
+        drawerContentStyleObj={styles.drawerContent}
+        offset={combinedHeight}
+        drawerPosition={'left'}
+        drawerSize={'lg'}
+        closeDrawerTriggerFunc={() => setMenuDrawer(false)}
+        drawerTriggerState={menuDrawer}
+      />
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    position: 'absolute',
+    left: 0,
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 0
+  }
+})
 
 export default MenuDrawer
