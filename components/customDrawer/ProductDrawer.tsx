20cdfd0fe9f0da6c5e329654b8f0afc23a4af393
miavai649 <mahmudulhaquenoor@gmail.com>
2025-01-06 17:35:00 +0600

    quick view drawer completed

--- components/customDrawer/ProductDrawer.tsx
+++ components/customDrawer/ProductDrawer.tsx
@@ -1,4 +1,4 @@
-import { Pressable } from 'react-native'
+import { Image, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import CustomDrawer from '.'
 import { DrawerBody, DrawerHeader } from '../ui/drawer'
@@ -11,6 +11,7 @@ import { VStack } from '../ui/vstack'
 import CartCard from '../card/CartCard'
 import { Text } from '../ui/text'
 import { View } from 'react-native'
+import CustomButton from '../button'
 
 interface ProductDrawerContentProps {
   setProductDrawer: (state: boolean) => void
@@ -29,7 +30,7 @@ const ProductDrawerContent = ({
   const [size, setSize] = useState('S')
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className='flex-1'>
       <DrawerHeader className='p-0 m-0'>
         <HStack className='items-center justify-between w-full'>
           <Heading className='text-2xl font-bold leading-[38.4px]'>
@@ -43,13 +44,16 @@ const ProductDrawerContent = ({
           </Pressable>
         </HStack>
       </DrawerHeader>
-      {/* border-1 */}
+      {/* Border Divider */}
       <Divider className='mt-6 border-[0.6px] border-opacity-[30%] border-[#F64343]' />
-      <DrawerBody className='p-0 m-0 mt-6'>
+
+      {/* Drawer Body */}
+      <DrawerBody className='flex-1 p-0 m-0 mt-6'>
         <VStack className='gap-6'>
-          {/* selected product */}
+          {/* Selected Product */}
           <CartCard product={productData} asQuickView={true} />
-          {/* border line-2 */}
+
+          {/* Divider */}
           <Divider className='mt-6 border-[0.6px] border-opacity-[30%] border-[#F64343]' />
 
           {/* product size select box */}
@@ -123,8 +127,54 @@ const ProductDrawerContent = ({
               </Pressable>
             </HStack>
           </View>
+          {/* color selection area */}
+          <View>
+            <Text className='text-xl font-normal leading-8'>Color</Text>
+            <HStack className='justify-between mt-2'>
+              <View>
+                <Image source={require('../../assets/images/color-1.png')} />
+                <Text className='text-sm font-normal leading-[21px]'>Red</Text>
+              </View>
+              <View>
+                <Image source={require('../../assets/images/color-2.png')} />
+                <Text className='text-sm font-normal leading-[21px]'>Red</Text>
+              </View>
+              <View>
+                <Image source={require('../../assets/images/color-3.png')} />
+                <Text className='text-sm font-normal leading-[21px]'>Red</Text>
+              </View>
+              <View>
+                <Image source={require('../../assets/images/color-4.png')} />
+                <Text className='text-sm font-normal leading-[21px]'>Red</Text>
+              </View>
+            </HStack>
+          </View>
         </VStack>
       </DrawerBody>
+
+      {/* Drawer Footer */}
+      <View className='absolute bottom-0 left-0 right-0 bg-white p-4'>
+        <Divider className='border-[0.6px] border-opacity-[30%] border-[#F64343]' />
+        <HStack className='justify-between mt-4'>
+          <Heading className='font-semibold text-2xl'>Subtotal</Heading>
+          <Heading className='font-bold text-2xl'>$130.38</Heading>
+        </HStack>
+        <Text className='text-[#555353] text-base mt-2'>
+          Taxes and shipping calculated at checkout
+        </Text>
+        <HStack className='justify-between gap-6 mt-4'>
+          <CustomButton
+            btnText='View Cart'
+            btnTextStyle='text-lg font-semibold text-center'
+            btnStyle='flex-1 rounded-none py-[10px] px-4 border border-[#0E0C0C]'
+          />
+          <CustomButton
+            btnText='Checkout'
+            btnTextStyle='text-lg font-semibold text-center text-[#FDFDFD]'
+            btnStyle='flex-1 rounded-none py-[10px] px-4 bg-[#F64343]'
+          />
+        </HStack>
+      </View>
     </SafeAreaView>
   )
 }
